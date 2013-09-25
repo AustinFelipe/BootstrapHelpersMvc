c7aa481fee7651288c433290dd55aacdae917ae7
Austin Felipe <austinfelipe@ig.com.br>
2013-09-25 17:39:15 -0300

    Added test unit

---
+++ BootTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BootstrapMvc.Base.Core;
+
+namespace BootTest
+{
+    [TestClass]
+    public class BaseTest
+    {
+        [TestMethod]
+        public void TestElementRender()
+        {
+            Element<Div> t = new Element<Div>("Test");
+            Assert.AreEqual(t.Render(), "<Test></Test>");
+        }
+    }
+}
